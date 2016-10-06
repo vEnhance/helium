2ec48a20c6a55d57db6a657d956b58949ad80217
Evan Chen <evanchen@mit.edu>
2016-10-06 07:45:50 -0400

    "He" should be capitalized

--- admin.py
+++ admin.py
@@ -1,38 +1,36 @@
 from django.contrib import admin
-import helium.models as he
-
-# Register your models here.
+import helium.models as He
 
 # Inline classes, so you can e.g. edit weight
 class ProblemInline(admin.TabularInline):
-    model = he.Problem
+    model = He.Problem
 class EvidenceInline(admin.TabularInline):
-    model = he.Evidence
+    model = He.Evidence
 class ProblemScribbleInline(admin.TabularInline):
-    model = he.ProblemScribble
+    model = He.ProblemScribble
 
-@admin.register(he.Test)
+@admin.register(He.Test)
 class TestAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color')
     inlines = (ProblemInline,)
 
-@admin.register(he.Problem)
+@admin.register(He.Problem)
 class ProblemAdmin(admin.ModelAdmin):
     list_display = ('id', 'test', 'problem_number', 'weight', 'allow_partial')
 
-@admin.register(he.TestScribble)
+@admin.register(He.TestScribble)
 class TestScribbleAdmin(admin.ModelAdmin):
     list_display = ('id', 'test', 'mathlete', 'team')
 
-@admin.register(he.Verdict)
+@admin.register(He.Verdict)
 class VerdictAdmin(admin.ModelAdmin):
     list_display = ('id', 'problem', 'mathlete', 'team', 'score', 'is_valid', 'is_done')
     inlines = (EvidenceInline, ProblemScribbleInline,)
 
-@admin.register(he.ProblemScribble)
+@admin.register(He.ProblemScribble)
 class ProblemScribbleAdmin(admin.ModelAdmin):
     list_display = ('id', 'verdict', 'problem_number', 'testscribble')
 
-@admin.register(he.Evidence)
+@admin.register(He.Evidence)
 class EvidenceAdmin(admin.ModelAdmin):
     list_display = ('id', 'verdict', 'user', 'score', 'god_mode')
