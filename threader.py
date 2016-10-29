f5e981d5a7b2623b692d83c476521dbc6937f014
Evan Chen <evanchen@mit.edu>
2016-10-29 12:02:02 -0400

    Log threading debug

--- threader.py
+++ threader.py
@@ -8,10 +8,15 @@ threader.py
 This is a simple wrapper for running processes async.
 """
 
+import logging
 import threading
 
-
-def run_async(func):
+def run_async(func, name=""):
+	def target_func():
+		try:
+			func()
+		else:
+			logging.info("%s completed OK" %name)
 	t = threading.Thread(target = func)
 	t.daemon = True
 	t.start()
