9ef7a2e1af24f819d2411a7c6ec45d1c490fa64a
Evan Chen <evanchen@mit.edu>
2016-11-23 13:10:13 -0500

    Make update scribbles usable

--- management/commands/updatescribbles.py
+++ management/commands/updatescribbles.py
@@ -3,11 +3,27 @@ import helium as He
 import itertools
 import logging
 
+from django.db.models import F
+
 class Command(BaseCommand):
 	help = "For every ExamScribble, runs updateScribbles on it if possible"
 
 	def handle(self, *args, **kwargs):
-		examscribbles = He.models.ExamScribble.objects\
-				.all().prefetch_related('problemscribble_set')
-		for es in examscribbles:
-			es.updateScribbles(es.problemscribble_set.all())
+		queryset = He.models.Verdict.objects.filter(problemscribble__isnull=False)\
+
+		queryset1 = queryset\
+				.filter(entity__isnull=True)\
+				.filter(problemscribble__examscribble__entity__isnull=False)
+		queryset2 = queryset\
+				.filter(entity__isnull=False)\
+				.filter(problemscribble__examscribble__entity__isnull=True)
+		queryset3 = queryset\
+				.filter(entity__isnull=False)\
+				.exclude(problemscribble__examscribble__entity = F("entity"))
+
+		n = 0
+		for v in itertools.chain(queryset1, queryset2, queryset3):
+			v.entity = v.problemscribble.examscribble.entity
+			v.save()
+			n += 1
+		return "%d updates" %n
