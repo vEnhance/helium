7f9c63c47cae1cca94c454f2e3d6c08f5fc347c3
Evan Chen <evanchen@mit.edu>
2018-11-09 09:16:16 -0500

    Optimize algscore with @transaction.atomic

--- management/commands/algscore.py
+++ management/commands/algscore.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 import collections
 
 from math import exp, log, floor
@@ -9,8 +10,8 @@ class Command(BaseCommand):
 	help = "Runs algorithmic scoring according to new algorithm, weighting all the individual problems."
 
 	def handle(self, *args, **kwargs):
-		problem_ids = He.models.Problem.objects\
-				.filter(exam__is_alg_scoring=True).values_list('id', flat=True)
+		problems = He.models.Problem.objects.filter(exam__is_alg_scoring=True)
+		problem_ids = problems.values_list('id', flat=True)
 
 		verdicts = He.models.Verdict.objects\
 				.filter(score__isnull=False, entity__isnull = False)\
@@ -23,13 +24,15 @@ class Command(BaseCommand):
 			if v['score']:
 				num_solves[v['problem__id']] += 1
 
-		for p_id in problem_ids:
-			problem = He.models.Problem.objects.get(id=p_id)
-			n = problem.problem_number
-			N = num_solves[p_id]
-			if N == 0:
-				w = 9.99
-			else:
-				w = exp(n/20.0) + max(8 - floor(log(N)), 2)
-			problem.weight = w
-			problem.save()
+		@transaction.atomic
+		def update_scores(queryset):
+			for problem in queryset:
+				n = problem.problem_number
+				N = num_solves[problem.id]
+				if N == 0:
+					w = 9.99
+				else:
+					w = exp(n/20.0) + max(8 - floor(log(N)), 2)
+				problem.weight = w
+				problem.save()
+		update_scores(problems)
